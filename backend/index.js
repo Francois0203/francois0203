49db447e48e34bbcfbeef29cb76e3e4daf28e82e
Francois <francoismeiring0203@gmail.com>
2024-12-12 22:47:55 +0200

    Backend - Frontend Integration Test 1
    Integration worked locally. Added CORS for it to work. Made some changes for it to work in production as well. (TEST 1)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const projectsRouter = require('./routes/projects'); // Import the projects router
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+// Middleware to enable CORS
+app.use(cors({
+  origin: ['http://localhost:5000', 'https://Francois0203.github.io/Francois0203/'], // Allow requests from your local frontend and deployed frontend
+  methods: 'GET, POST',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 // Middleware to parse JSON
 app.use(express.json());
