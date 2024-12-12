caa587f6d76b4f79e575b11dad9235e7d694954f
Francois <francoismeiring0203@gmail.com>
2024-12-12 14:52:46 +0200

    Backend Host Test 1
    Deploy backend to Render, and set up CI/CD pipeline using github actions.

--- backend/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "https://francois0203.github.io/Francois0203"}})
-    return app
