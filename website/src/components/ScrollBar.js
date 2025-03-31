d888e4623a2b5fa069805063b514432f8056aca9
Francois <francoismeiring0203@gmail.com>
2025-03-31 21:37:53 +0200

    Complete Restart

--- website/src/components/ScrollBar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./ScrollBar.module.css";
-
-const ScrollBar = ({ children }) => {
-  return <div className={styles.scrollbarContainer}>{children}</div>;
-};
-
-export default ScrollBar;
